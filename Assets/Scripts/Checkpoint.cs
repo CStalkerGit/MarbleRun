b35f61e4470307625d321298027438d2070a7eab
CStalker <cstalker.coder@gmail.com>
2022-08-04 01:42:59 +0300

    respawn areas

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Transform RespawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            var trigger = other.GetComponent<RespawnTrigger>();
+            if (trigger) trigger.RespawnPoint = RespawnPoint;
+        }
+    }
+}
--- Assets/Scripts/RespawnTrigger.cs
+++ Assets/Scripts/RespawnTrigger.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class RespawnTrigger : MonoBehaviour
 {
     public Transform RespawnPoint;
 
     void FixedUpdate()
     {
-        if (transform.position.y < 2.0f)
+        if (transform.position.y < -2.0f && RespawnPoint)
         {
             transform.position = RespawnPoint.position;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
     }
 }
