364407873acc4c8ff5a04562e36780f1c036851f
CStalker <cstalker.coder@gmail.com>
2022-08-08 23:14:49 +0300

    new platforms

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -13,7 +13,6 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Test");
         transform.Rotate(0, 150 * Time.deltaTime, 0);
     }
 
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public bool Moving;
+    public bool Rotating;
+
+    bool movingLeft;
+    float maxZ = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movingLeft = transform.position.z > 0;
+        maxZ = Mathf.Abs(transform.position.z);
+    }
+
+    void Update()
+    {
+        if (Rotating) transform.Rotate(0, 50 * Time.deltaTime, 0);
+        if (Moving)
+        {
+            var pos = transform.position;
+            var length = 5 * Time.deltaTime;
+            if (movingLeft)
+            {
+                pos.z -= length;
+                if (pos.z < -maxZ) movingLeft = false;
+            } 
+            else
+            {
+                pos.z += length;
+                if (pos.z > maxZ) movingLeft = true;
+            }
+            transform.position = pos;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
